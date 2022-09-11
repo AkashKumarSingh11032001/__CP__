bfe1f9db8ce22a16ba568c01121f8d8ccc34f222
Akash <akashkumarsingh11032001@gmail.com>
2022-09-11 10:26:29 +0530

    pattern 2

--- 1_Pattern/pattern.cpp
+++ 1_Pattern/pattern.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 /* PATTERN 1
@@ -7,18 +7,44 @@ using namespace std;
 1234
 1234
 */
-void pattern_1(int n){
-    for(int i = 0; i < n; i++){
-        for(int j = 1;j<=n; j++){
+void pattern_1(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
             cout << j << " "; // for reverse use n-j+1
         }
-        cout<< endl;
+        cout << endl;
     }
-
 }
 
-int main(){
+/* PATTERN 2
+123
+456
+789
+*/
+void pattern_2(int n) //n = no. of row
+{
+    int val = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << val++ << " "; 
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
 
     // pattern 1
     pattern_1(6);
+
+    cout << endl;
+
+    // pattern 2
+    pattern_2(3);
 }
