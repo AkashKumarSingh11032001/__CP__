5f2cfd7aa80c809a44860c19b134687fb4c9e33e
Akash <akashkumarsingh11032001@gmail.com>
2022-09-11 10:34:28 +0530

    Pattern 4

--- 1_Pattern/pattern.cpp
+++ 1_Pattern/pattern.cpp
@@ -24,27 +24,73 @@ void pattern_1(int n)
 456
 789
 */
-void pattern_2(int n) //n = no. of row
+void pattern_2(int n) // n = no. of row
 {
     int val = 1;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            cout << val++ << " "; 
+            cout << val++ << " ";
         }
         cout << endl;
     }
 }
 
+/* PATTERN 3
+*
+**
+***
+****
+*/
+void pattern_3(int n) // n = no. of row
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+}
+
+/* PATTERN 4
+1
+22
+333
+4444
+*/
+void pattern_4(int n) // n = no. of row
+{
+    for (int i = 1; i <= n; i++)
+    {
+        int val = i;
+        for (int j = 1; j <= i; j++)
+        {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
 int main()
 {
 
     // pattern 1
-    pattern_1(6);
+    // pattern_1(6);
 
     cout << endl;
 
     // pattern 2
-    pattern_2(3);
+    // pattern_2(3);
+
+    cout << endl;
+
+    // pattern 3
+    // pattern_3(3);
+
+    cout << endl;
+
+    // pattern 4
+    pattern_4(4);
 }
