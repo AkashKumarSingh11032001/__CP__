3c597cf0b07081c4b07560dc8652ec24899ed97c
Akash <akashkumarsingh11032001@gmail.com>
2022-10-07 15:41:46 +0530

    ./

---
+++ future.py
@@ -0,0 +1,4 @@
+# iops = int(
+    #     "".join(i for i in x if i.isdigit())
+    # )  # sometime the value is in 2 digit and in some rare case its in 1 digit so, or in some case it will combination of alpha,some special charter with number so we will only pick digit out of it.
+    # return digi
