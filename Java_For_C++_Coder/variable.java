50694e647a421ac797359d6bf574f2f5a5fc596b
Akash <akashkumarsingh11032001@gmail.com>
2022-10-07 19:27:55 +0530

    variable

---
+++ Java_For_C++_Coder/variable.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class variable {
+
+    public static void main(String[] args) {
+
+        
+        System.out.println(Math.pow(2, 3)); //float
+        System.out.println((int)Math.pow(2, 3)); //int
+        
+    }
+}
