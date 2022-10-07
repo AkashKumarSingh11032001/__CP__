0fb8f7eb88dc123e6a75aa76fc60d60581537b96
Akash <akashkumarsingh11032001@gmail.com>
2022-10-07 20:28:03 +0530

    fibonacci Series

---
+++ Java_For_C++_Coder/fibonacciSeries.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class fibonacciSeries {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        System.out.print(a);
+        System.out.print(b);
+        while(n != 0){
+            int c = a + b;
+            System.out.print(c);
+            a = b;
+            b = c;
+            n = n -1;
+        }
+        
+    }
+}
